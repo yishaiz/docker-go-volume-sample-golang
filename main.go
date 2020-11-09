f70b70437fb0114263a78b51a36cf8336a31b408
yishai <yishai5555@gmail.com>
2020-11-09 10:07:09 +0200

    03-add-redis-error

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"fmt"
+	redis "gopkg.in/redis.v4"
+)
 
-import "fmt"
 func main() {
-    fmt.Println("Hello World")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
 }
+
+// import "fmt"
+// func main() {
+//     fmt.Println("Hello World")
+// }
 
 // import (
 // 	"fmt"
